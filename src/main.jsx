4cbfbb16917365fa78d092fdde5189f42ea179b4
yili <hellomickey004@gmail.com>
2024-04-24 17:56:03 +0800

    feat: signIn router, change page components and buttons into Pages and Buttons file

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import HomePage from './Components/HomePage/index.jsx'
-import GamePage from './Components/GamePage/index.jsx'
-import InvitePage from './Components/InvitePage/index.jsx'
+import HomePage from './Components/Pages/HomePage'
+import GamePage from './Components/Pages/GamePage'
+import InvitePage from './Components/Pages/InvitePage'
+import SignInPage from './Components/Pages/SignInPage/index.jsx'
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -14,6 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route index element={<HomePage />} />
         <Route path="chouchouzoo/:roomId/:roomName" element={<GamePage />} />
         <Route path="invite/:roomId/:roomName" element={<InvitePage />} />
+        <Route path="signIn" element={<SignInPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
