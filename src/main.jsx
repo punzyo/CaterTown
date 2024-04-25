32c6230d75b97ec1abdd263bc399690b97f2bcea
yili <hellomickey004@gmail.com>
2024-04-25 22:05:18 +0800

    feat: get more map images

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,9 @@ import InvitePage from './Components/Pages/InvitePage';
 import SignUpPage from './Components/Pages/SignUpPage/index.jsx';
 import SignInPage from './Components/Pages/SiginInPage/index.jsx';
 import CheckAuth from './Components/Pages/CheckAuth/index.jsx';
+import MapEditor from './Components/Pages/MapEditor/index.jsx';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import MapImages from './Components/Pages/MapEditor/MapImages/index.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -19,6 +21,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path="invite/:roomId/:roomName" element={<InvitePage />} />
           <Route path="signup" element={<SignUpPage />} />
           <Route path="signin" element={<SignInPage />} />
+          <Route path="map" element={<MapEditor />} />
+          <Route path="mapImages" element={<MapImages />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
       </Route>
