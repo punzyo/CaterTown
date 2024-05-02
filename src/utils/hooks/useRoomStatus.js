80e05b0ff191a2a19659ed0ded08f5f01655c31d
yili <hellomickey004@gmail.com>
2024-05-02 21:08:35 +0800

    fix:online status function

--- src/utils/hooks/useRoomStatus.js
+++ src/utils/hooks/useRoomStatus.js
@@ -1,31 +1,29 @@
 import { useEffect, useState } from 'react';
-import { ref, onDisconnect, onValue, update, getDatabase } from 'firebase/database';
+import { ref, onDisconnect, onValue, update } from 'firebase/database';
 import { useNavigate } from 'react-router-dom';
+import { rtdb } from '../../firebase/realtime';
 
 export function useRoomStatus({ userId, roomId }) {
     const [onlineStatus, setOnlineStatus] = useState({});
     const navigate = useNavigate();
-    const db = getDatabase();
 
     useEffect(() => {
-        const userStatusRef = ref(db, `rooms/${roomId}/users/${userId}`);
+        const userStatusRef = ref(rtdb, `rooms/${roomId}/users/${userId}`);
 
-        // 设置用户在线，并确保不覆盖其他数据
         update(userStatusRef, { online: true });
         onDisconnect(userStatusRef).update({ online: false });
 
-        const roomUsersRef = ref(db, `rooms/${roomId}/users`);
+        const roomUsersRef = ref(rtdb, `rooms/${roomId}/users`);
         const unsubscribe = onValue(roomUsersRef, snapshot => {
             const users = snapshot.val() || {};
             setOnlineStatus(users);
         });
 
-        // 组件卸载时设置用户离线
         return () => {
             unsubscribe();
-            update(userStatusRef, { online: false });  // 更新，而不是设置，以保持其他数据
+            update(userStatusRef, { online: false });  
         };
-    }, [userId, roomId, db, navigate]);
+    }, [userId, roomId, rtdb, navigate]);
 
     return onlineStatus;
 }
