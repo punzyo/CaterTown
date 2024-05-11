16b9003ec974451c1ad44c68973f68b899e9b785
yili <hellomickey004@gmail.com>
2024-05-11 21:03:42 +0800

    feat: communication tutorial

--- src/Components/Pages/GamePage/PlayerProfile/Tutorial/index.jsx
+++ src/Components/Pages/GamePage/PlayerProfile/Tutorial/index.jsx
@@ -28,17 +28,18 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
-.slick-slide{
- >div{
-  height: 420px;
-
- }  
-}
+  .slick-slide {
+    > div {
+      height: 420px;
+    }
+  }
   .slick-slider {
     width: 80%;
     height: 90%;
     color: black;
-    .slick-slide>div{padding: 0 10px;}
+    .slick-slide > div {
+      padding: 0 10px;
+    }
     h2 {
       font-size: 36px;
       text-align: center;
@@ -105,6 +106,69 @@ const Title = styled.h2`
   width: 100%;
   padding: 0 20px;
 `;
+const sliderContent = {
+  basic: [
+    {
+      title: "角色移動",
+      content: "可以透過WASD，或是方向鍵的上、下、左、右對角色進行移動，碰到障礙物或是地圖邊界時則會停下。",
+      mediaType:'video'
+    },
+    {
+      title: "獲取邀請連結",
+      content: "點擊上方的圖示可以獲得此房間的邀請連結。"
+    },
+    {
+      title: "控制側邊欄",
+      content: "點擊下方欄的圖示以隱藏/顯示側邊欄。"
+    },
+    {
+      title: "離開房間",
+      content: "點擊右下角圖示即可離開房間。"
+    },
+    {
+      title: "傳遞文字訊息",
+      content: "可以透過全體頻道向所有人發送訊息，或是點擊他人頭像進行一對一的聊天。"
+    },
+    {
+      title: "打開個人面板",
+      content: "這裡可以輸入您的GitHub，如果權限為teacher以上，可以使用權限相關的系統。"
+    },
+    {
+      title: "Pull request系統",
+      content: "當設置好 GitHub webhook後，若您的角色有至少一則pull request，頭上將會冒出驚嘆號通知，自己或是權限為teacher以上的角色都會看到。"
+    }
+  ],
+  communication:[
+    {
+      title: "加入多人通訊",
+      content: "按下多人通訊的按鈕，連線成功後即可以進行多人視/音訊連線。"
+    }, {
+      title: "視/音訊距離",
+      content: "當進入多人通訊後，只要角色彼此在兩格內的距離，就能看到對方的通訊畫面。"
+    }, {
+      title: "房間系統",
+      content: "島上共有四個獨立的空間，房間內的角色只能接收同個空間的視/音訊，無視距離。"
+    },{
+      title: "通訊裝置選擇",
+      content: "可以點擊圖標右側的按鈕選擇視音訊的輸入源。"
+    },
+    
+    {
+      title: "螢幕分享",
+      content: "點擊下方螢幕圖示可以對畫面進行分享。"
+    },
+  
+    {
+      title: "全螢幕，音量控制",
+      content: "點擊視訊畫面右下角可以進行全螢幕，左下角則是音量控制。"
+    },
+    {
+      title: "退出多人通訊",
+      content: "點擊右方的按鈕將會離開多人通訊，將不會發送/接收通訊內容。"
+    }
+  ]
+};
+
 export default function Tutorial() {
   const BASIC = 'basic';
   const COMMUNICATION = 'communication';
@@ -117,6 +181,7 @@ export default function Tutorial() {
     slidesToShow: 1,
     slidesToScroll: 1,
   };
+  const currentContent = sliderContent[category];
   return (
     <Mask
       onClick={(e) => {
@@ -151,85 +216,21 @@ export default function Tutorial() {
             權限功能
           </Category>
         </CategoryWrapper>
-        <Slider {...settings}>
-          <div key={0}>
-            <Title>角色移動</Title>
-            <video src="/tutorial/basic/01.mp4"></video>
-            <p>
-              可以透過WASD，或是方向鍵的上、下、左、右對角色進行移動，碰到障礙物或是地圖邊界時則會停下。
-            </p>
+        <Slider {...settings} key={category}>
+        {currentContent.map((item, index) => (
+          <div key={index}>
+            <Title>{item.title}</Title>
+            {item.mediaType === 'video' ? (
+              <video src={`/tutorial/${category}/${category}_${index}.mp4`} />
+            ) : (
+              <img src={`/tutorial/${category}/${category}_${index}.png`} alt="" />
+            )}
+            <p>{item.content}</p>
           </div>
-          <div key={1}>
-            <Title>獲取邀請連結</Title>
-            <img src="/tutorial/basic/basic_02.png" alt="" />
-            <p>點擊上方的圖示可以獲得此房間的邀請連結</p>
-          </div>
-          <div key={2}>
-            <Title>控制側邊欄</Title>
-            <img src="/tutorial/basic/basic_03.png" alt="" />
-            <p>點擊下方欄的圖示以隱藏/顯示側邊欄</p>
-          </div>
-          <div key={4}>
-            <Title>離開房間</Title>
-            <img src="/tutorial/basic/basic_04.png" alt="" />
-            <p>點擊右下角圖示即可離開房間</p>
-          </div>
-          
-          <div key={5}>
-            <Title>打開個人面板</Title>
-            <img src="/tutorial/basic/basic_05.png" alt="" />
-            <p>
-              這裡可以輸入您的GitHub，如果權限為teacher以上，可以使用權限相關的系統。
-            </p>
-          </div>
-          <div key={6}>
-            <Title>傳遞文字訊息</Title>
-            <img src="/tutorial/basic/basic_06.png" alt="" />
-            <p>
-              可以透過全體頻道向所有人發送訊息，或是點擊他人頭像進行一對一的聊天
-            </p>
-          </div>
-          
-          <div key={19}>
-            <Title>Pull request系統</Title>
-            <p>
-              當設置好 repository webhook後，若您的角色open/reopen一則pull
-              request，頭上將會冒出驚嘆號通知，自己或是權限為teacher以上的角色都會看到，
-              點擊後會看到PR標題，可藉由點選PR標題前往該PR頁面。
-            </p>
-          </div>
-          <div key={50}>
-            <Title>加入多人通訊</Title>
-            <p>按下多人通訊的按鈕，連線成功後即可以進行多人視/音訊連線</p>
-          </div>
-          <div key={8}>
-            <Title>視/音訊距離</Title>
-            <p>
-              當進入多人通訊後，只要角色彼此在兩格內的距離，就能看到對方的通訊畫面
-            </p>
-          </div>
-          <div key={9}>
-            <Title>房間系統</Title>
-            <p>
-              島上共有四個獨立的空間，房間內的角色只能接收同個空間的視/音訊，無視距離
-            </p>
-          </div>
-          <div key={10}>
-            <Title>螢幕分享</Title>
-            <p>點擊下方螢幕圖示可以對畫面進行分享</p>
-          </div>
-          <div key={11}>
-            <Title>通訊裝置選擇</Title>
-            <p>可以點擊圖標右側的按鈕選擇視音訊的輸入源</p>
-          </div>
-          <div key={12}>
-            <Title>全螢幕，音量控制</Title>
-            <p>點擊視訊畫面右下角可以進行全螢幕，左下角則是音量控制</p>
-          </div>
-          <div key={13}>
-            <Title>退出多人通訊</Title>
-            <p>點擊右方的按鈕將會離開多人通訊，將不會發送/接收通訊內容</p>
-          </div>
+        ))}
+      </Slider>
+        {/* <Slider>
+         
           <div key={14}>
             <Title>通訊裝置選擇</Title>
             <p>可以點擊圖標右側的按鈕選擇視音訊的輸入源</p>
@@ -268,7 +269,7 @@ export default function Tutorial() {
               只有房間創始者可以將成員權限提升到admin，以及降階admin的成員，admin成員可以將teacher降階為student，teacher最多只能將student提升為teacher。
             </p>
           </div>
-        </Slider>
+        </Slider> */}
       </Wrapper>
     </Mask>
   );
